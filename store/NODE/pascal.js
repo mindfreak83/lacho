edb5fee6afeb02522175965f8a4ba61d356aee86
mindfreak83 <montalvo.jonathan@hotmail.com>
2017-02-01 17:53:55 -0600

    traducción del libro de html5

--- store/NODE/pascal.js
+++ store/NODE/pascal.js
@@ -13,7 +13,6 @@ function pascal (n) {
       ary = pascal(n-1);
       var val = Array.from (ary[n-2]);
       for (let x = 0; x < n; x++) {
-        debugger;
            if (x === 0 || x === n-1) {
                aux.push(1);
            } else {
